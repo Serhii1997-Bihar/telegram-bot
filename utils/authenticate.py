956abe3e6ebb76f25b3e5fe96c07c64fb4be8118
Serhii Bihar <bigar.ser21@gmail.com>
2025-11-20 14:40:17 +0200

    Added admin success and dotenv

--- utils/authenticate.py
+++ utils/authenticate.py
@@ -1,4 +1,8 @@
 from database.init_sqlite import get_db_connection
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 def is_registered(user_id):
     with get_db_connection() as conn:
@@ -8,7 +12,7 @@ def is_registered(user_id):
 
 
 def is_admin(user_id):
-    if user_id == 6227193529:
+    if user_id == os.getenv('MY_ID'):
         return True
 
     with get_db_connection() as conn:
